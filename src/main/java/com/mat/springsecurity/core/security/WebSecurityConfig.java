dcf26b639ca9184f9ec489331b14f8c31c80b274
MateusAlvarenga <mateus_13wp@hotmail.com>
2022-04-09 20:16:25 -0300

    usuario em memória

--- src/main/java/com/mat/springsecurity/core/security/WebSecurityConfig.java
+++ src/main/java/com/mat/springsecurity/core/security/WebSecurityConfig.java
@@ -1,21 +1,41 @@
 package com.mat.springsecurity.core.security;
 
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-@EnableWebSecurity
+@Configuration @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws Exception {
+    @Override @SneakyThrows
+    protected void configure(AuthenticationManagerBuilder auth){
+         auth.inMemoryAuthentication()
+                    .withUser("MAT").password(passwordEncoder().encode("123456")).roles("ADMIN")
+                .and()
+                    .withUser("caixa").password(passwordEncoder().encode("123456")).roles("USER");
+    }
+
+    @Override @SneakyThrows
+    protected void configure(HttpSecurity http)  {
         http.httpBasic()
                 .and()
                     .authorizeRequests()
-
-                         .anyRequest().authenticated();
-
-
+                         .anyRequest().authenticated()
+                .and()
+                    .sessionManagement()
+                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
